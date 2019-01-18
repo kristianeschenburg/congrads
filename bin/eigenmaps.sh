6624bcd66253d8ad750a8559883304f0b1a6c2f7
Kristian Eschenburg <keschenb@uw.edu>
2019-01-18 11:39:07 -0800

    eigenmaps script

---
+++ bin/eigenmaps.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+subject=$1
+source_region=$2
+regions=$3
+
+while read p
+do
+
+python eigenmaps.py -s ${subject} \
+-r /mnt/parcellator/parcellation/HCP/Connectome_4/${subject}/Split_Surface_ROIS/Desikan_Killiany/func/L.${source_region}.func.gii \
+-sim /mnt/parcellator/parcellation/parcellearning/Data/Connectopy/Regional/${subject}/${source_region}/${subject}.L.${source_region}.2.${p}.Eta2.mat \
+-dir /mnt/parcellator/parcellation/parcellearning/Data/Connectopy/Regional/${subject}/${source_region}/ \
+-o ${subject}.L.${source_region}.2.${p}.Evecs.func.gii
+
+done <${regions}
