aee34513837e998903b57c3bf6bc626347c50bc2
kristianeschenburg <keschenb@uw.edu>
2019-02-28 23:16:02 -0800

    cluster etas

---
+++ bin/eta2_regions.py
@@ -0,0 +1,98 @@
+import argparse
+import numpy as np
+from niio import loaded
+import os
+from fragmenter import RegionExtractor as re
+
+from congrads import conmap
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--subject', help='Input subject name.', required=True, type=str)
+parser.add_argument('-f', '--features', help='Feature data file.', required=True, type=str)
+parser.add_argument('-l', '--label', help='Label file.', required=True, type=str)
+parser.add_argument('-sr', '--sroi', help='Source rois.', required=True, type=str, nargs='+')
+parser.add_argument('-tr', '--troi', help='Target rois.', required=False, type=str, default=None, nargs='+')
+parser.add_argument('-d', '--dir', help='Output directory.', required=True, type=str)
+parser.add_argument('-bo', '--base_out', help='Base output name, without extension.', required=True, type=str)
+
+args = parser.parse_args()
+
+if not os.path.exists(args.dir):
+    print('Output directory does not exist -- creating now.')
+    os.mkdir(args.dir)
+
+# Load region of interest
+try:
+    assert os.path.isfile(args.label)
+except:
+    raise('Label file does not exist.')
+else:
+    label = loaded.load(args.label)
+    R = re.Extractor(args.label)
+    region_map = R.map_regions()
+
+sindices=[]
+tindices=[]
+
+for sr in args.sroi:
+    sindices.append(region_map[sr])
+sindices = np.concatenate(sindices)
+
+if args.troi:
+   target_exists=True
+   for tr in args.troi:
+      tindices.append(region_map[tr])
+   tindices = np.concatenate(tindices)
+else:
+   target_exists=False
+
+# Load feature matrix
+try:
+    assert os.path.isfile(args.features)
+except:
+    raise('Feature file for {:} does not exist.'.format(args.subject))
+else:
+    print ('Loading feature data.')
+    F = loaded.load(args.features)
+    n,p = F.shape
+
+    if n < p:
+        F = F.T
+
+F = (F-F.mean(1)[:, None]) / (F.std(1)[:, None])
+
+A = F[sindices, :]
+print('ROI shape: {:}'.format(A.shape))
+
+print('Transpose to generate time X samples matrix.')
+A = A.T
+
+if target_exists:
+    B = F[tindices, :]
+else:
+    B = F[sindices, :]
+
+zeros = np.isnan(np.abs(B).sum(1))
+B = B[~zeros, :]
+
+print('Target shape: {:}'.format(B.shape))
+print('Transpose target to generate time X samples matrix.')
+B = B.T
+
+print('Computing voxel-wise connectivity fingerprints...')
+[evecs, Bhat, evals] = conmap.pca(B)
+R = conmap.corr(A, Bhat)
+
+print('Computing similarity matrix.')
+E2 = conmap.eta2(R)
+
+print('Saving correlation and eta^2 matrix.')
+import scipy.io as sio
+r = {'r2': R}
+e = {'eta2': E2}
+
+fext_eta = '{:}{:}.L.Eta2.{:}.mat'.format(args.dir, args.subject, args.base_out)
+fext_cor = '{:}{:}.L.Corr.{:}.mat'.format(args.dir, args.subject, args.base_out)
+
+sio.savemat(file_name=fext_cor, mdict=r)
+sio.savemat(file_name=fext_eta, mdict=e)
---
+++ bin/spectral_eta2_distance.py
@@ -0,0 +1,94 @@
+import argparse
+import nibabel as nb
+import numpy as np
+
+from sklearn.cluster import SpectralClustering as spect
+
+from fragmenter import RegionExtractor as re
+import networkx as nx
+from surface_utilities import adjacency as adj
+
+from niio import loaded, write
+from congrads import conmap
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--subject', help='Subject ID.', required=True, type=str)
+parser.add_argument('-l', '--label', help='Label file.', required=True, type=str)
+parser.add_argument('-surf', '--surface', help='Surface file.', required=True, type=str)
+parser.add_argument('-e', '--eta', help='Eta2 file.', required=True, type=str)
+parser.add_argument('-c', '--clusters', help='Cluster range.', required=True,
+    type=int, nargs='+')
+parser.add_argument('-r', '--rois', help='ROI names.', required=True, type=str,
+    nargs='+')
+
+parser.add_argument('-hops', '--hop_distance', help='Maximum hop distance.',
+    required=True, type=int)
+
+parser.add_argument('-d', '--dir', help='Output directory.', required=True, type=str)
+parser.add_argument('-bo', '--outbase', help='Output base name.', required=True, type=str)
+
+print('Generating region map...')
+args = parser.parse_args()
+label = loaded.load(args.label)
+R = re.Extractor(args.label)
+region_map = R.map_regions()
+
+print('Loading eta matrix...')
+eta = loaded.load(args.eta)
+eta[np.isinf(eta)] = 0
+eta[np.isnan(eta)] = 0
+
+cmin = args.clusters[0]
+cmax = args.clusters[1]
+
+indices = []
+for r in args.rois:
+    indices.append(region_map[r])
+indices = np.concatenate(indices)
+sort_inds = np.argsort(indices)
+
+print('Generating adjacency matrix...')
+surf = nb.load(args.surface)
+vertices = surf.darrays[0].data
+faces = surf.darrays[1].data
+S = adj.SurfaceAdjacency(vertices=vertices, faces=faces)
+S.generate(indices=indices)
+
+adjmat = np.zeros((len(indices), len(indices)))
+index2coords = dict(zip(list(S.adj.keys()), np.arange(len(indices))))
+coords2index = dict(zip(index2coords.values(), index2coords.keys()))
+
+for k, v in S.adj.items():
+    for n in v:
+        adjmat[index2coords[k], index2coords[n]] = 1
+
+print('Generating distance matrix...')
+G = nx.from_numpy_array(adjmat)
+apsp = nx.floyd_warshall_numpy(G)
+
+distmat = np.zeros((len(indices), len(indices)))
+for dist in np.arange(1, args.hop_distance):
+
+    print('Clustering at distance: {:}'.format(dist))
+
+    distmat = (apsp<=dist)
+
+    sorted_eta = eta[:, sort_inds]
+    sorted_eta = sorted_eta[sort_inds, :]
+    sorted_eta = sorted_eta*distmat
+
+    for c in np.arange(cmin, cmax+1):
+
+        print('Clusters: {:}'.format(c))
+
+        S = spect(n_clusters=c, affinity='precomputed')
+        S.fit(sorted_eta)
+
+        labs = S.labels_
+        labs[labs==0] = (labs.max() + 1)
+
+        z = np.zeros((label.shape))
+        z[indices[sort_inds]] = labs
+
+        out_path = '{:}{:}.L.{:}.Cluster.{:}.Distance.{:}.func.gii'.format(args.dir, args.subject, args.outbase, c, dist)
+        write.save(z, out_path, 'CortexLeft')
